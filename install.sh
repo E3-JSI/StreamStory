202f749bebc56c73344eda20a9997d265a717fc6
Luka Stopar <luka.stopar@ijs.si>
2015-09-14 11:57:11 +0200

    added config for the demo

--- install.sh
+++ install.sh
@@ -11,16 +11,18 @@ sudo apt-get install uuid-dev
 sudo apt-get install libopenblas-dev
 sudo apt-get install liblapacke
 sudo apt-get install liblapacke-dev
+sudo apt-get install curl
+sudo apt-get install build-essential
+sudo apt-get install libssl-dev
 
 # install node
 echo 'Installing Node.js ...'
-sudo apt-get install curl build-essential libssl-dev
-
 if [ `type -t nvm` == 'function' ]; then
 	echo 'NVM already installed!'
 else
 	echo 'Installing NVM ...'
-	curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh
+	#curl https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh
+	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
 	source ~/.profile
 fi
 
@@ -41,12 +43,14 @@ echo 'Building qminer ...'
 cd qminer
 node-gyp clean && node-gyp configure -- -DLIN_ALG_BLAS=BLAS -DLIN_ALG_LAPACKE=LAPACKE -DLIN_ALG_LIB=-llapacke
 node-gyp --verbose build
+npm install
 cd ..
 
 # create the database
 echo 'Creating database. Please enter your MySQL root password'
 cd StreamStory
 cat init.sql | mysql -u root -p
+npm install
 cd ..
 
 echo 'Done!'
