723872e26c0476f62be5caef4216e7cb8447e18f
Luka Stopar <luka.stopar@ijs.si>
2015-09-14 08:53:48 +0200

    ...

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# install prereqisites
+sudo apt-get install python2.7
+sudo apt-get install git
+
+# install node
+# TODO
+
+# install npm
+sudo apt-get update
+sudo apt-get install npm
+sudo npm update
+sudo apt-get install uuid-dev
+
+# install LAPACKE
+sudo apt-get install liblapacke
+sudo apt-get install liblapacke-dev
