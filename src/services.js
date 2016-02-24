1543ddb9443685ccbccd6668cd244973ac6d062b
Luka Stopar <luka.stopar@ijs.si>
2016-02-24 19:02:43 +0100

    ...

--- src/services.js
+++ src/services.js
@@ -236,7 +236,7 @@ function addRawMeasurement(val) {
 		var prevTimestamp = storeLastTm[storeNm];
 		
 		if (totalCounts++ % config.RAW_PRINT_INTERVAL == 0 && log.debug())
-			log.debug('Received raw data ...');
+			log.debug('Received raw data, inserting into store %s ...', storeNm);
 		if (timestamp <= prevTimestamp)
 			throw new Error('Invalid time for a single measurement: ' + timestamp + ' <= ' + prevTimestamp);
 		if (timestamp < lastRawTime)
