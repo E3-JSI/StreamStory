899a76c0d94a30759ddfd62fae93af9ac3cc56bf
Luka Stopar <luka.stopar@ijs.si>
2016-10-25 16:21:51 +0200

    fixed internal storage for dockerization

--- docker/env/nissa.sh
+++ docker/env/nissa.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# host directories
+# HOST DIRECTORIES
+# directory of StreamStory's configuration file
 export CONFIG_DIR=/home/proasense/app/streamstory-config
+# MySQL storage directory
 export DATABASE_DIR=/home/proasense/app/streamstory-db
+# StreamStory storage directory
+export MODELS_DIR=/home/proasense/app/streamstory-internal
 
+# PORTS
 export STREAMSTORY_PORT=8080
 export DATABASE_PORT=3308
