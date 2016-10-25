07e22b112a20f4775714c259b244f5089475fd89
Luka Stopar <luka.stopar@ijs.si>
2016-10-25 12:00:50 +0200

    ...

--- init-db.sql
+++ init-db.sql
@@ -2,4 +2,6 @@
 CREATE DATABASE StreamStory;
 ALTER DATABASE StreamStory CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER 'StreamStory'@'localhost' IDENTIFIED BY 'StreamStory';
+CREATE USER 'StreamStory'@'%' IDENTIFIED BY 'StreamStory';
 GRANT ALL PRIVILEGES ON StreamStory.* TO 'StreamStory'@'localhost';
+GRANT ALL PRIVILEGES ON StreamStory.* TO 'StreamStory'@'%';
