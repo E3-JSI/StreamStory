bbc8b9ed3312ad541ace6b14e0e7cb30766e9828
Luka Stopar <luka.stopar@ijs.si>
2017-02-15 14:09:02 +0100

    ...

---
+++ src/ssmodules.js
@@ -0,0 +1,93 @@
+//==================================
+// USER MANAGER
+//==================================
+
+/**
+ * StreamStory user manager. Manages all user related stuff.
+ *
+ * @param {object} opts
+ * @property [StreamStoryDatabase] opts.db - the database interface
+ */
+// function UserManager(opts) {
+//     var self = this;
+
+//     self._db = opts.db;
+// }
+
+
+//==================================
+// MODEL MANAGER
+//==================================
+
+function ModelManager(opts) {
+    var self = this;
+    self._db = opts.db;
+    self._modelStore = opts.modelStore;
+}
+
+ModelManager.prototype.sendMessage = function (model, message, callback) {
+    var self = this;
+    self._modelStore.sendMsg(model.getId(), JSON.stringify(message));
+    self.storeMessage(model, message, callback);
+}
+
+ModelManager.prototype.storeMessage = function (model, message, callback) {
+    var self = this;
+
+    var modelId = model.getId();
+    self._db.storeMessage(modelId, message, callback);
+}
+
+/**
+ * Fetches the latest messages for the given user.
+ *
+ * @param {object} user - the user
+ * @param {number} limit - max number of messages
+ * @param {funciton} callback - standard callback
+ */
+ModelManager.prototype.getLatestMessages = function (model, limit, callback) {
+    var self = this;
+
+    if (model == null) return callback(new Error('Model missing!'));
+    if (limit == null || limit <= 0) return callback(new Error('Invlid limit value: ' + limit)) ;
+
+    var modelId = model.getId();
+    var since = new Date(Date.now() - 1000*60*60*24*7);
+
+    var opts = {
+        mid: modelId,
+        startTm: since,
+        limit: limit
+    }
+    self._db.fetchLatestModelMessages(opts, function (e, messages) {
+        if (e != null) return callback(e);
+        callback(undefined, self._processOutputMessages(messages));
+    });
+}
+
+/**
+ * Returns the count of all the active models in the system.
+ *
+ * @param {function} callback - standard callback
+ */
+ModelManager.prototype.countTotalActive = function (callback) {
+    var self = this;
+    self._db.countActiveModels(function (e, result) {
+        if (e != null) return callback(e);
+        callback(undefined, result.count);
+    });
+}
+
+ModelManager.prototype._processOutputMessages = function (messages) {
+    var result = [];
+    for (var i = 0; i < messages.length; i++) {
+        result.push(messages[i].content);
+    }
+    return result;
+}
+
+//==================================
+// EXPORTS
+//==================================
+
+exports.ModelManager = ModelManager;
